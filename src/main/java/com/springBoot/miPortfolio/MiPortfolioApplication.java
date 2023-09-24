0155fae1b30e4735732ffbbb4615e1c43fc6a857
Kevin Soto <dev@kevinsoto.ar>
2023-09-24 19:52:49 -0300

    Set CORS allowed origins in application.properties

--- src/main/java/com/springBoot/miPortfolio/MiPortfolioApplication.java
+++ src/main/java/com/springBoot/miPortfolio/MiPortfolioApplication.java
@@ -1,27 +1,35 @@
 package com.springBoot.miPortfolio;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MiPortfolioApplication {
 
+    @Value("${miportfolio.web.origins}")
+    private String[] allowedOrigins;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MiPortfolioApplication.class, args);
 	}
         
-        @Bean
-        public WebMvcConfigurer corsConfigurer(){
-            return new WebMvcConfigurer(){
-                @Override
-                public void addCorsMappings(CorsRegistry registry){
-                    registry.addMapping("/**").allowedOrigins("https://mi-portfolio-f6a2e.web.app/").allowedHeaders("*").allowedMethods("*");
-                    
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                CorsRegistration corsRegistration = registry.addMapping("/**").allowedHeaders("*").allowedMethods("*");
+
+                for (String origin : allowedOrigins) {
+                    corsRegistration.allowedOrigins(origin);
                 }
-            };
-        }
+            }
+        };
+    }
 
 }
